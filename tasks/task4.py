68ee4e3b89e68be7302232f5b4ae4117223fc5ad
Сергей Сергиенко <sergo100@yandex.ru>
2025-10-15 04:32:37 +0500

    добавил решения в ветку solution

---
+++ tasks/task4.py
@@ -0,0 +1,13 @@
+# tasks/task4.py
+
+def solve():
+# Ниже пишите решение задачи
+    
+    a = input()
+    b = input()
+    print(b + a)
+
+
+# Код ниже не трогать! он нужен для тестов
+if __name__ == "__main__":
+    solve()
